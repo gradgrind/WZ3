8c5ece336e1160c3b77d81ea63b77620ece9e635
gradgrind <mt.bothfeld@gmail.com>
2023-03-25 08:33:44 +0100

    Work on course_data

--- wz/core/course_data.py
+++ wz/core/course_data.py
@@ -38,64 +38,15 @@ if __name__ == "__main__":
 
 ### +++++
 
-from typing import NamedTuple
+from typing import Optional
 from core.db_access import (
-    db_read_unique,
     db_read_full_table,
-    db_update_field,
-    db_update_fields,
-    db_new_row,
-    db_delete_rows,
-    db_values,
     db_read_unique_entry,
-    NoRecord,
 )
-from core.teachers import Teachers
-from core.classes import Classes
 from core.basic_data import (
     Workload,
-    clear_cache,
-    get_subjects,
     BlockTag,
-    ParallelTag,
 )
-
-#TODO ...???
-
-# SUBJECT, CLASS and TEACHER are foreign keys with:
-#  on delete cascade + on update cascade
-FOREIGN_FIELDS = ("CLASS", "TEACHER", "SUBJECT")
-
-COURSE_TABLE_FIELDS = ( # the fields shown in the course table
-# (db-field name, column-type, horizontal text alignment)
-# column-type:
-#   -1: checkbox
-#    0: db-value
-#    1: display-value (from column-dependent map)
-# alignment:
-#   -1: left
-#    0: centre
-#    1: right
-    ("CLASS", 0, 0),
-    ("GRP", 0, 0),
-    ("SUBJECT", 1, -1),
-    ("TEACHER", 1, -1),
-    ("REPORT", -1, 0),
-    ("GRADES", -1, 0),
-    ("INFO", 0, -1),
-)
-
-class LessonRowData(NamedTuple):
-    """ROW_TYPE:
-        -2 – no item (all other fields <None>)
-        -1 – workload/payment item (only COURSE_LESSON_INFO not <None>)
-         0 – "normal" lesson group (not a block)
-         1 – block lesson group
-    """
-    ROW_TYPE: int
-    COURSE_LESSON_INFO: dict
-    LESSON_GROUP_INFO: dict
-    LESSON_INFO: dict
 
 ### -----
 
@@ -113,43 +64,34 @@ def load_course_table(filter, value):
     return courses
 
 
-def course_activities(course_id):
+def course_activities(course_id:int
+) -> tuple[Optional[tuple], Optional[tuple], list[dict]]:
     """Seek lessons and workload/pament info for the given course
     (<course_id>).
+    There can be a (single) workload/payment entry.
+    There can be a (single) group of simple lessons.
+    There can be named blocks – with distinct (non-empty)
+    BLOCK_SID/BLOCK_TAG values.
+
+    Return: (workload/payment element, simple element, block elements)
+
+    NOTE how the parameters are set in various tables. The room-wish
+    and pay details apply to all lesson components as they are set in
+    COURSE_LESSONS. Only the time-wish is set in the lesson component.
+    This may be a bit restrictive, but is perhaps reasonable for most
+    cases. If it is really essential to have a particular room for a
+    particular lesson (and another one, or a choice, for another
+    lesson), perhaps some additional constraint could be added ...
     """
+    workload_element = None
+    simple_element = None
+    block_elements = []
     fields, records = db_read_full_table(
         "COURSE_LESSONS", course=course_id
     )
-    ## Build a list of entries, loop through entries in COURSE_LESSONS.
-    ## There can be a (single) workload/pament entry.
-    ## There can be a (single) group of simple lessons.
-    ## There can be named blocks.
-    course_lessons = []
-
-# DETAILS:
-# There should be only one COURSE_LESSONS entry for "simple lesson"
-# types and "workload/payment" types. For "block lesson" types there
-# can be more than one entry, but they should be connected with
-# LESSON_GROUP entries with distinct (non-empty) BLOCK_x values.
-# If violations are discovered, there should be an error report. It
-# might be helpful to delete the offending entries, but as they are
-# really not expected – and should not be possible – it is perhaps
-# better to report the offending entries and not to delete them, so
-# that they are available for debugging purposes – the report could
-# be via a bug exception?
-
-# Also note how the parameters are set in various tables. The room
-# wish and pay details apply to all lesson components as they are set in
-# COURSE_LESSONS. Only the time wish is set in the lesson component.
-# This may be a bit restrictive, but is perhaps reasonable for most
-# cases. If it is really essential to have a particular room for a
-# particular lesson (and another one, or a choice, for another lesson),
-# perhaps some additional constraint could be added ...
-
-    workload_element = False
-    simple_element = False
     for rec in records:
         cldict = {fields[i]: val for i, val in enumerate(rec)}
+        workload_data = Workload(**cldict)
         # <cldict> contains workload/payment and room-wish fields
         lg = cldict["lesson_group"]
         if lg:
@@ -168,39 +110,33 @@ def course_activities(course_id):
             # The uniqueness of a course/lesson_group connection
             # should be enforced by the UNIQUE constraint on the
             # COURSE_LESSONS table ("course" + "lesson_group" fields).
+            lfields, lrecords = db_read_full_table(
+                "LESSONS", lesson_group=lg
+            )
+            lessons = [
+                {lfields[i]: val for i, val in enumerate(lrec)}
+                for lrec in lrecords
+            ]
             if block_sid:
-                etype = 1
                 bt = BlockTag.build(block_sid, block_tag)
-                lgdata["BlockTag"] = bt
+                block_elements.append(
+                    (workload_data, cldict, lgdata, lessons, bt)
+                )
             else:
                 if simple_element:
                     raise Bug(
                         "Multiple entries in COURSE_LESSONS"
                         f"for simple lesson item, course {course_id}"
                     )
-                simple_element = True
-                etype = 0
-            lfields, lrecords = db_read_full_table(
-                "LESSONS", lesson_group=lg
-            )
-            lgdata["nLessons"] = len(lrecords)
-            for lrec in lrecords:
-                ldata = {lfields[i]: val for i, val in enumerate(lrec)}
-                ln = ldata["LENGTH"]
-                course_lessons.append(
-                    LessonRowData(etype, cldict, lgdata, ldata)
-                )
+                simple_element = (workload_data, cldict, lgdata, lessons)
         else:
             # payment/workload item
             if workload_element:
                 raise Bug("Multiple entries in COURSE_LESSONS"
-                    f"for workload item, course {self.course_id}"
+                    f"for workload item, course {course_id}"
                 )
-            workload_element = True
-            course_lessons.append(
-                LessonRowData(-1, cldict, None, None)
-            )
-    return course_lessons
+            workload_element = (workload_data, cldict)
+    return (workload_element, simple_element, block_elements)
 
 
 # --#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#--#
@@ -210,6 +146,18 @@ if __name__ == "__main__":
     open_database()
 
     for course in load_course_table("TEACHER", "AE"):
-        print("\n\n *** COURSE:", course)
-        for l in course_activities(course["course"]):
-            print("\n  ---", l)
+        print("\n\n *** COURSE:", course["course"], course)
+        w, l, b = course_activities(course["course"])
+        if w:
+            print("  ***", str(w[0]), w[1:])
+        else:
+            print("  ***")
+        if l:
+            print("  ---", str(l[0]), l[1:])
+        else:
+            print("  ---")
+        if b:
+            for bi in b:
+                print("  +++", str(bi[0]) or "-$-", bi[1:])
+        else:
+            print("  +++ []")
